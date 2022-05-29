9a77cbc46bffb4f6f441e160ba7b0217ba2fcda8
Alexandre Galdeano <alexandre.galdeano@aibstract.fr>
2022-05-29 16:29:35 +0200

    Added filters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,27 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <h1 class="title">Rummy Tracker <v-btn icon @click="reset()"><v-icon>mdi-sync</v-icon></v-btn></h1>
+        <h1 class="title">Rummy Tracker
+          <v-btn icon @click="reset()">
+            <v-icon>mdi-sync</v-icon>
+          </v-btn>
+        </h1>
       </v-col>
-      <v-col v-for="(card, cardIndex) of deck.cards" :key="cardIndex" cols="6" sm="4" md="3" lg="2">
+      <v-col cols="12">
+        <v-btn icon v-for="(value, valueIndex) of allCardValues" :key="valueIndex" @click="filterValue(value)"
+               :color="value === filters.value ? 'primary' : ''" class="ma-1">
+          <v-img v-if="value === 'joker'" src="img/joker.svg" height="30"/>
+          <span v-else-if="['ace', 'jack', 'queen', 'king'].includes(value)">
+            {{ value[0] }}
+          </span>
+          <span v-else>{{ value }}</span>
+        </v-btn>
+        <v-btn icon v-for="(type, typeIndex) of allCardTypes" :key="typeIndex" @click="filterType(type)"
+               :color="type === filters.type ? 'primary' : ''"  :class="['ma-1', iconColor(type)]">
+          <v-icon>mdi-cards-{{ type }}</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col v-for="(card, cardIndex) of filteredCards" :key="cardIndex" cols="6" md="2" sm="3" xl="1">
         <CardComponent :card="card"/>
       </v-col>
     </v-row>
@@ -23,22 +41,67 @@ import CardComponent from '@/components/CardComponent.vue'
   components: { CardComponent },
 })
 export default class HomeView extends Vue {
+  filters: {
+    value?: CardValue,
+    type?: CardType
+  } = {}
+
   private values = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'] as CardValue[]
 
   readonly deck = new Deck(
       [
-          ...$enum(CardType)
-          .map((type) => this.values.map((value) => new Card(value, type)))
-          .flat()
-          .map((card) => new CardStatus(card, 2)),
+        ...$enum(CardType)
+            .map((type) => this.values.map((value) => new Card(value, type)))
+            .flat()
+            .map((card) => new CardStatus(card, 2)),
         new CardStatus(new Card('joker'), 4),
       ],
   )
+
+  get allCardValues(): CardValue[] {
+    return ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'joker'] as CardValue[]
+  }
+
+  get allCardTypes(): CardType[] {
+    return $enum(CardType).map((it) => it)
+  }
 
   reset(): void {
     this.deck.cards.forEach((card) => {
       card.resetPlayed()
     })
   }
+
+  get filteredCards(): CardStatus[] {
+    return this.deck.cards.filter((it) => {
+      const matchesValue = this.filters.value ? it.value === this.filters.value : true
+      const matchesType = this.filters.type ? it.type === this.filters.type : true
+      return matchesValue && matchesType
+    })
+  }
+
+  filterValue(value: CardValue): void {
+    const currentFilterValue = this.filters.value
+    this.filters.value = currentFilterValue === value ? undefined : value
+  }
+
+  filterType(type: CardType): void {
+    const currentFilterType = this.filters.type
+    this.filters.type = currentFilterType === type ? undefined : type
+  }
+
+  iconColor(type: CardType): string {
+    return [CardType.CLUB, CardType.SPADE].includes(type) ? 'black' : 'red'
+  }
 }
 </script>
+
+<style lang="scss">
+.red i {
+  color: #F44336;
+}
+
+.black i {
+  color: #212121;
+}
+</style>
